d37f857adb73080aba02862d5a9150200559b0f0
NneomaOkoye <okoyenneoma1@gmail.com>
2023-08-17 05:23:30 -0700

    done

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,12 +1,12 @@
-# Fixing problems in high amounts
+# Enhance the maximum file limit for the Holberton user (hard limit).
 
-file { 'increase-hard-file-limit-for-holberton-user':
+exec { 'elevate-hard-file-limit-for-holberton-user':
   command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf',
   path    => '/usr/local/bin/:/bin/'
 }
 
-# Increase soft file limit for Holberton user.
-file { 'increase-soft-file-limit-for-holberton-user':
+# Augment the allowable file limit for the Holberton user (soft limit).
+exec { 'augment-soft-file-limit-for-holberton-user':
   command => 'sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf',
   path    => '/usr/local/bin/:/bin/'
 }
