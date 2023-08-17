45f2fcdb37fefe6038dacd6559fb2f03d9323b27
NneomaOkoye <okoyenneoma1@gmail.com>
2023-08-17 05:29:38 -0700

    done

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,10 +1,11 @@
-# let us start fixing the high request problems
+# Elevate the ULIMIT value for the default file
 
 exec {'adjust--for-nginx':
-  command => 'sed -i "s/15/4096/" /etc/default/nginx',
+  command => 'sed -i"s/15/4096/" /etc/default/nginx',
   path => '/usr/local/bin/:/bin/'
 } ->
 
+# Reboot Nginx to apply changes
 exec {'restart-nginx':
   command => 'nginx restart',
   path => '/etc/init.d/'
