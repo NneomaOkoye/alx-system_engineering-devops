8d430121ae7d0a7b9cd24f5b085f3c1b033ccea0
NneomaOkoye <okoyenneoma1@gmail.com>
2023-08-17 04:41:07 -0700

    done

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,12 +1,12 @@
 # let us start fixing the high request problems
 
-file {'replace':
+exec {'replace':
   provider => shell,
   command  => 'sudo sed -i "s/ULIMIT=\"-n 15\"/ULIMIT=\"-n 4096\"/" /etc/default/nginx',
   before   => Exec['restart'],
 }
 
-service {'restart':
+exec {'restart':
   provider => shell,
   command  => 'sudo service ngnix restart',
 }
