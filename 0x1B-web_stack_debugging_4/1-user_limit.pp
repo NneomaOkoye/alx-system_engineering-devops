19b410f2dfc54c4ab961edb7c60a1dec5f161eaa
NneomaOkoye <okoyenneoma1@gmail.com>
2023-08-17 04:19:50 -0700

    done

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,12 +1,13 @@
-# Fixing high amount of problems of opened files
+# Fixing something
 
 file {'replace-1':
   provider => shell,
   command  => 'sudo sed -i "s/nofile 5/nofile 50000/" /etc/security/limits.conf',
-  before   => Exec['replace-2'],
+  path   => Exec['replace-2'],
 }
 
 service {'replace-2':
   provider => shell,
   command  => 'sudo sed -i "s/nofile 4/nofile 40000/" /etc/security/limits.conf',
+  path     => '/usr/local/bin/:/bin/'
 }  
